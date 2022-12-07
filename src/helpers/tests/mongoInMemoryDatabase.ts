873c520c50fb2dc541de08df5b8e72b4f3e1c4a5
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2022-12-07 14:31:28 -0300

    test: configurando mongodb memory server

---
+++ src/helpers/tests/mongoInMemoryDatabase.ts
@@ -0,0 +1,59 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+
+class MongoInMemoryDatabase {
+  private mongoServer?: MongoMemoryServer;
+  private static instance: MongoInMemoryDatabase;
+
+  private constructor() {}
+
+  public static getInstance() {
+    if (!MongoInMemoryDatabase.instance) {
+      MongoInMemoryDatabase.instance = new MongoInMemoryDatabase();
+    }
+    return MongoInMemoryDatabase.instance;
+  }
+
+  public async open() {
+    try {
+      this.mongoServer = await MongoMemoryServer.create();
+
+      const uri = this.mongoServer.getUri();
+      await mongoose.connect(uri);
+    } catch (error) {
+      console.log("Failed to create database.");
+      console.log(error);
+      throw error;
+    }
+  }
+  public async close() {
+    try {
+      await mongoose.connection.dropDatabase();
+      await mongoose.disconnect();
+      if (this.mongoServer) {
+        await this.mongoServer.stop();
+      }
+    } catch (error) {
+      console.log("Failed to close to database.");
+      console.log(error);
+      throw error;
+    }
+  }
+
+  public async clear() {
+    try {
+      const collections = mongoose.connection.collections;
+      for (const nameCollection in collections) {
+        const collection = collections[nameCollection];
+        await collection.deleteMany({});
+      }
+    } catch (error) {
+      console.log("Failed to clean the collections in memory.");
+      console.log(error);
+      throw error;
+    }
+  }
+
+}
+
+export const mongoInMemoryDatabase = MongoInMemoryDatabase.getInstance();
