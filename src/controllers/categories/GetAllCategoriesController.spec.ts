78f60cd0200893c82083a96595381d13e7eb9bc2
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2022-12-08 20:09:16 -0300

    test: busca de categorias

---
+++ src/controllers/categories/GetAllCategoriesController.spec.ts
@@ -0,0 +1,37 @@
+import request from "supertest";
+import { App } from "../../App";
+import { mongoInMemoryDatabase } from "./../../helpers/tests/mongoInMemoryDatabase";
+
+describe("Delete Post Controller", () => {
+  beforeAll(async () => {
+    await mongoInMemoryDatabase.open();
+  });
+
+  afterAll(async () => {
+    await mongoInMemoryDatabase.close();
+  });
+
+  beforeEach(async () => {
+    await mongoInMemoryDatabase.createCategory();
+  });
+
+  const app = new App();
+
+  it("Should return a 200 status if the requisition is successful", async () => {
+    await request(app.getExpress).get("/categories").expect(200);
+  });
+
+  it("Should return an empty array if there are no categories in the database", async () => {
+    await mongoInMemoryDatabase.clear();
+
+    const response = await request(app.getExpress).get("/categories").expect(200);
+    const {data} = response.body
+    expect(data.length).toBe(0);
+  });
+
+  it("Should return 'Route not found' if the url is not valid", async () => {
+    const response = await request(app.getExpress).get("/categoriessa")
+    const {message} = response.body
+    expect(message).toEqual("Rota não encontrada");
+  });
+});
