2258391764b460124c84555dee7c4520b79078e5
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2022-12-12 22:24:44 -0300

    test: atualização de conteúdo digital

---
+++ src/controllers/digitalContents/UpdateDigitalContentController.spec.ts
@@ -0,0 +1,91 @@
+import { mongoInMemoryDatabase } from "./../../helpers/tests/mongoInMemoryDatabase";
+import { fileURLToPath } from "url";
+import { App } from "../../App";
+import request from "supertest";
+import path from "path";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.resolve(__dirname, "..", "..", "..", "temps", "uploads");
+const fileMock = path.resolve(__dirname, "..", "..", "..", "temps", "tests", "4495.jpg");
+
+describe("UpdateDigitalContent", () => {
+  beforeAll(async () => {
+    await mongoInMemoryDatabase.open();
+  }, 60_000);
+
+  afterAll(async () => {
+    await mongoInMemoryDatabase.close();
+  });
+
+  beforeEach(async () => {
+    await mongoInMemoryDatabase.createCategory();
+    await mongoInMemoryDatabase.createDigitalContent();
+  });
+
+  afterEach(async () => {
+    await mongoInMemoryDatabase.clear();
+    fs.rmSync(filePath, { recursive: true, force: true });
+  });
+
+  const app = new App();
+
+  it("Should be able to update a digital content", async () => {
+    const digitalContent = await mongoInMemoryDatabase.getDigitalContent();
+    const category = await mongoInMemoryDatabase.getCategory();
+    const { guide } = category;
+    const response = await request(app.getExpress)
+      .put(`/digital-contents/${digitalContent._id}`)
+      .field(
+        "data",
+        JSON.stringify({
+          title: `Título do conteúdo digital atualizado`,
+          shortDescription: `Descrição do conteúdo digital atualizado`,
+          guide: guide._id,
+          category: category._id,
+        }),
+      );
+    expect(response.statusCode).toBe(200);
+    const { title } = response.body.data;
+    expect(title).toEqual("Título do conteúdo digital atualizado");
+  });
+
+  it("Must not update a guide without its id", async () => {
+    const category = await mongoInMemoryDatabase.getCategory();
+    const { guide } = category;
+    const response = await request(app.getExpress)
+      .put(`/digital-contents/`)
+      .field(
+        "data",
+        JSON.stringify({
+          title: "`Título do conteúdo digital`",
+          shortDescription: `Descrição do conteúdo digital atualizado`,
+          guide: guide._id,
+          category: category._id,
+        }),
+      )
+      .attach("files", fileMock);
+    expect(response.statusCode).toBe(404);
+  });
+
+  it("Must not update a digital content with invalid id", async () => {
+    const digitalContent = await mongoInMemoryDatabase.getDigitalContent();
+    const category = await mongoInMemoryDatabase.getCategory();
+    const { guide } = category;
+    const response = await request(app.getExpress)
+      .put(`/digital-contents/${digitalContent._id}232`)
+      .field(
+        "data",
+        JSON.stringify({
+          title: `Título do conteúdo digital`,
+          shortDescription: `Descrição do conteúdo digital`,
+          guide: guide._id,
+          category: category._id,
+        }),
+      )
+      .attach("files", fileMock);
+    expect(response.statusCode).toBe(500);
+  });
+});
