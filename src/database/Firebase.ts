4048d6eae67422ce38a57a22cc1ab6e54c71ec5b
Rhannamilena <rhannamillena@gmail.com>
2022-12-19 20:22:49 -0300

    configuração de autenticação com o Firebase

---
+++ src/database/Firebase.ts
@@ -0,0 +1,19 @@
+import firebase from "firebase-admin";
+
+const credentials = JSON.stringify({
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+});
+
+const firebaseApp = firebase.initializeApp({
+  credential: firebase.credential.cert(JSON.parse(credentials)),
+});
+export const firebaseAuth = firebaseApp.auth();
