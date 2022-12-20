3907d8363d8626da19448fe4454dec6235f6a067
Rhannamilena <rhannamillena@gmail.com>
2022-12-20 10:49:37 -0300

    Novo model Mongoose

---
+++ src/models/settings/userSchemaSettings.ts
@@ -0,0 +1,14 @@
+export const userSchemaSettings = {
+  uid: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+};
