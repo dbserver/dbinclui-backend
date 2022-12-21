6532e04513fbf8709216232486a22f604eb5c21c
lucasantoniodev <lucasantoniodev@gmail.com>
2022-12-21 13:09:28 -0300

    test: CreateUserController add default user

--- src/controllers/users/CreateUserController.spec.ts
+++ src/controllers/users/CreateUserController.spec.ts
@@ -22,6 +22,7 @@ describe("CreateUserController", () => {
   beforeAll(async () => {
     firebase.initializeApp();
     await mongoInMemoryDatabase.open();
+    await mongoInMemoryDatabase.createUser();
     express = new App();
   }, 60_000);
 
@@ -90,7 +91,7 @@ describe("CreateUserController", () => {
     const token =
       "https://jwt.io/#debugger-io?token=eyJhbGciOiJSUzI1NiIsImtpZCI6ImFmZjFlNDJlNDE0M2I4MTQxM2VjMTI1MzQwOTcwODUxZThiNDdiM2YiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiTHVjYXMgQW50w7RuaW8iLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUVkRlRwNUNLcXhEMEprLWc1VDdhSjhmUWJIYTdCMDAtU0c1c2pBaFBZVnQ9czk2LWMiLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vbGVhcm5pbmctZmlyZWJhc2UtMzZkMWIiLCJhdWQiOiJsZWFybmluZy1maXJlYmFzZS0zNmQxYiIsImF1dGhfdGltZSI6MTY3MTU4NjIyMSwidXNlcl9pZCI6InlpeFp0bTNnUHVmRzdreThidXZiV1p0OXl2bTIiLCJzdWIiOiJ5aXhadG0zZ1B1Zkc3a3k4YnV2YldadDl5dm0yIiwiaWF0IjoxNjcxNTg2MjIxLCJleHAiOjE2NzE1ODk4MjEsImVtYWlsIjoidGVjb25mdEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJnb29nbGUuY29tIjpbIjExMzE0MjQ3ODk1MTIxMzI0MjQ0MSJdLCJlbWFpbCI6WyJ0ZWNvbmZ0QGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6Imdvb2dsZS5jb20ifX0.lCP2jgndfF_8RYeRDsCeFFP0ctSUAbzT6kH6mJF8kxnxmI2hBWdDYnHDBFr_jm7vSr-nXM9faYpJGtmdEIe1LkI2VSbnkgyMVCpuVrO0dWslo9MtKLDtnjYFesB420pTqH4D_OF0HCeHzu0FUWHgU2P03bzqmgt43J2MVv-JGlrJN2iWvWRUOxA2ekNBDRvNkzgzJ4RrDhImNd7XyYByMSX5f1AWQ7qaQj8wsr6wpsXtyDbnQ4VRtQrcyVzp3WVITXfr0bgblYopRO-0-CsQBjMOpCoFL4ILq8-YP-aIjBVzV7k95bF6HvVjBywlvXicbcf_K2J8GJzpyVHBkcWZ4A";
     verifyIdTokenMock.mockReturnValue({
-      uid: "123456",
+      uid: "123",
       name: "Joaozinho",
       email: "Joaozinho@email.com",
     });
