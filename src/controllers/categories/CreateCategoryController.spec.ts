b3bde681784b0db9e31765bf2835d46dcd8cc435
lucasantoniodev <lucasantoniodev@gmail.com>
2022-12-23 09:39:47 -0300

    test: add firebase-admin

--- src/controllers/categories/CreateCategoryController.spec.ts
+++ src/controllers/categories/CreateCategoryController.spec.ts
@@ -1,17 +1,32 @@
+import { jest } from "@jest/globals";
+import firebase from "firebase-admin";
 import request from "supertest";
 import { App } from "../../App";
 import { mongoInMemoryDatabase } from "./../../helpers/tests/mongoInMemoryDatabase";
 
+const verifyIdTokenMock = jest.fn();
+
+jest.mock("firebase-admin/auth", () => ({
+  getAuth: jest.fn().mockImplementation(() => {
+    return {
+      verifyIdToken: verifyIdTokenMock,
+    };
+  }),
+}));
+
 describe("CreateCategoryController", () => {
   beforeAll(async () => {
+    firebase.initializeApp();
     await mongoInMemoryDatabase.open();
   }, 60_000);
 
   afterAll(async () => {
+    await firebase.app().delete();
     await mongoInMemoryDatabase.close();
   });
 
   afterEach(async () => {
+    verifyIdTokenMock.mockClear();
     await mongoInMemoryDatabase.clear();
   });
 
@@ -22,30 +37,50 @@ describe("CreateCategoryController", () => {
   const app = new App();
 
   it("Should be able to create a new category", async () => {
+    const token = "validToken";
+
+    verifyIdTokenMock.mockReturnValue({});
+
     const guide = await mongoInMemoryDatabase.getGuide();
-    await request(app.getExpress).post("/categories").send({
-      title: "Título da categoria",
-      shortDescription: "Descrição da categoria",
-      guide: guide._id,
-    });
-    expect(200);
+    await request(app.getExpress)
+      .post("/categories")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "Título da categoria",
+        shortDescription: "Descrição da categoria",
+        guide: guide._id,
+      })
+      .expect(200);
   });
 
   it("Must not be able to create a new category with title longer than 32 characters", async () => {
+    const token = "validToken";
+    verifyIdTokenMock.mockReturnValue({});
+
     const guide = await mongoInMemoryDatabase.getGuide();
-    await request(app.getExpress).post("/categories").send({
-      title: "Título da categoria maior do que o esperado",
-      shortDescription: "Descrição da categoria",
-      guide: guide._id,
-    });
-    expect(400);
+    await request(app.getExpress)
+      .post("/categories")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "Título da categoria maior do que o esperado",
+        shortDescription: "Descrição da categoria",
+        guide: guide._id,
+      })
+      .expect(400);
   });
 
   it("Must not allow the registration of a category without a guide", async () => {
-    const response = await request(app.getExpress).post("/categories").send({
-      title: "Título da categoria",
-      shortDescription: "Descrição da categoria",
-    });
+    const token = "validToken";
+
+    verifyIdTokenMock.mockReturnValue({});
+
+    const response = await request(app.getExpress)
+      .post("/categories")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "Título da categoria",
+        shortDescription: "Descrição da categoria",
+      });
     const { msg } = response.body.message[0];
     expect(response.status).toBe(400);
     expect(msg).toEqual("É necessário passar um guia para criar uma categoria");
