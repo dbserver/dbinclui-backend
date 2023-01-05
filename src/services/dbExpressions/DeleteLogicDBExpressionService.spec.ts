3e166336ba3cfee0e66033311842fde2777d1251
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-01-04 21:03:50 -0300

    test: delete logic dbExpression service

---
+++ src/services/dbExpressions/DeleteLogicDBExpressionService.spec.ts
@@ -0,0 +1,39 @@
+import { DBExpressionEntity } from "./../../entities/DBExpressionEntity";
+import { inMemoryDBExpressionsRepository } from "./../../helpers/inMemoryRepositories/inMemoryDBExpressionsRepository";
+import { DeleteLogicDBExpressionService } from "./DeleteLogicDBExpressionService";
+
+describe("DeleteLogicDBExpressionService", () => {
+  let InMemoryDBExpressionsRepository: inMemoryDBExpressionsRepository;
+  let dbExpressionService: DeleteLogicDBExpressionService;
+
+  beforeAll(() => {
+    InMemoryDBExpressionsRepository = new inMemoryDBExpressionsRepository();
+    dbExpressionService = new DeleteLogicDBExpressionService(InMemoryDBExpressionsRepository);
+  });
+
+  beforeEach(() => {
+    InMemoryDBExpressionsRepository.loadExpresionDefaultData(3);
+  });
+
+  afterEach(() => {
+    InMemoryDBExpressionsRepository.clearAllDatabases();
+  });
+
+  it("Should return an error if an expressionId does not exist", async () => {
+    const result = (await dbExpressionService.execute("InexistenteID", "12")) as DBExpressionEntity;
+
+    expect(result).toBeInstanceOf(Error);
+  });
+
+  it("Should delete a expression by ID and return a DBExpressionEntity expression", async () => {
+    const result = (await dbExpressionService.execute("0", "1")) as DBExpressionEntity;
+
+    console.log(result);
+    expect(result._id).toHaveProperty("0");
+    expect(result).toHaveProperty("expression");
+    expect(result).toHaveProperty("author");
+    expect(result).toHaveProperty("deleted");
+    expect(result).toHaveProperty("favoriteOf");
+    expect(result).toHaveProperty("updatedBy");
+  });
+});
