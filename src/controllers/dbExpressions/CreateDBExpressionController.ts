e5cdb2e801c0eadc78cf9639820d16c181036cc3
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-01-05 11:49:11 -0300

    fix: rename req.body.user to req.currentUser

--- src/controllers/dbExpressions/CreateDBExpressionController.ts
+++ src/controllers/dbExpressions/CreateDBExpressionController.ts
@@ -1,29 +1,33 @@
 import { Request, Response } from "express";
 import { DBExpressionsMongoRepository } from "../../repositories/mongoRepositories/DBExpressionsMongoRepository.js";
 import { CreateDBExpressionService } from "../../services/dbExpressions/CreateDBExpressionService.js";
-import { sucessfulResponse, serverErrorResponse, clientErrorResponse } from "../../responses/appResponses.js";
+import {
+  sucessfulResponse,
+  serverErrorResponse,
+  clientErrorResponse,
+} from "../../responses/appResponses.js";
 import { UserEntity } from "../../entities/UserEntity";
 
 class CreateDBExpressionController {
-    async handler(req: Request, res: Response) {
-        try {
-            const expression = req.body.expression;
-            const author = req.body.user as UserEntity;
+  async handler(req: Request, res: Response) {
+    try {
+      const expression = req.body.expression;
+      const author = req.currentUser;
 
-            const dbExpressionsRepository = new DBExpressionsMongoRepository();
-            const dbExpressionService = new CreateDBExpressionService(dbExpressionsRepository);
+      const dbExpressionsRepository = new DBExpressionsMongoRepository();
+      const dbExpressionService = new CreateDBExpressionService(dbExpressionsRepository);
 
-            const result = await dbExpressionService.execute({ expression, author });
+      const result = await dbExpressionService.execute({ expression, author });
 
-            if (result instanceof Error) {
-                return clientErrorResponse(res, result);
-            }
+      if (result instanceof Error) {
+        return clientErrorResponse(res, result);
+      }
 
-            return sucessfulResponse(res, { data: result });
-        } catch (error) {
-            return serverErrorResponse(res, error as Error);
-        }
+      return sucessfulResponse(res, { data: result });
+    } catch (error) {
+      return serverErrorResponse(res, error as Error);
     }
+  }
 }
 
 export const createDBExpressionController = new CreateDBExpressionController();
