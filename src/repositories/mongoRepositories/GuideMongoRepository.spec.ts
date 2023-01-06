e29a748227dc4c580e0194457f40a666dc970a33
annablsantos <annabea608@gmail.com>
2023-01-06 15:11:47 -0300

    feature: GuideMongoRepository.spec.ts

--- src/repositories/mongoRepositories/GuideMongoRepository.spec.ts
+++ src/repositories/mongoRepositories/GuideMongoRepository.spec.ts
@@ -90,4 +90,80 @@ describe("GuideMongoRepository", () => {
       expect(result).toHaveProperty("filePaths");
     });
   });
+
+  describe("FindById", () => {
+    it("Should return null if ID doest not exist", async () => {
+      const result = await repository.findById("00b0c00a1b506ae265806e0e");
+
+      expect(result).toBeNull();
+    });
+
+    it("Should return a guide", async () => {
+      const guide: GuideEntity[] = await repository.findAll();
+      const guideID = guide[0]._id as string;
+
+      const result = await repository.findById(guideID);
+
+      expect(result).toHaveProperty("_id", guideID);
+      expect(result).toHaveProperty("title", "Título default");
+      expect(result).toHaveProperty("content", "Descrição default");
+      expect(result).toHaveProperty("author");
+      expect(result).toHaveProperty("deleted", false);
+      expect(result).toHaveProperty("filePaths");
+    });
+  });
+
+  describe("findAll", () => {
+    it("Should return all guides", async () => {
+      const result = await repository.findAll();
+
+      expect(result[0]).toHaveProperty("_id");
+      expect(result[0]).toHaveProperty("title");
+      expect(result[0]).toHaveProperty("content");
+      expect(result[0]).toHaveProperty("author");
+      expect(result[0]).toHaveProperty("deleted", false);
+      expect(result[0]).toHaveProperty("filePaths");
+      expect(result.length).toEqual(1);
+    });
+  });
+
+  describe("DeleteLogic", () => {
+    it("Should delete logic and return a category with delete true", async () => {
+      const guide: GuideEntity[] = await repository.findAll();
+      const guideID = guide[0]._id as string;
+
+      const result = await repository.deleteLogic(guideID, "63b5c66a1b506ae265806e6e");
+
+      expect(result).toHaveProperty("_id", guideID);
+      expect(result).toHaveProperty("title", "Título default");
+      expect(result).toHaveProperty("content", "Descrição default");
+      expect(result).toHaveProperty("author");
+      expect(result).toHaveProperty("deleted", true);
+      expect(result).toHaveProperty("filePaths");
+    });
+  });
+
+  describe("Delete", () => {
+    it("Should delete permanently a guide", async () => {
+      const guide: GuideEntity[] = await repository.findAll();
+      const guideID = guide[0]._id as string;
+
+      const result = await repository.delete(guideID);
+
+      const guides: GuideEntity[] = await repository.findAll();
+
+      expect(result).toHaveProperty("_id", guideID);
+      expect(result).toHaveProperty("title", "Título default");
+      expect(result).toHaveProperty("content");
+      expect(result).toHaveProperty("author");
+      expect(result).toHaveProperty("deleted", false);
+      expect(result).toHaveProperty("filePaths");
+    });
+  });
+
+  describe("FindCategoriesAndContentsByGuideId",  () =>{})
+  it("Should find categories and digital contents", async () =>{
+    const result = await repository.findCategoriesAndContentsByGuideId("")
+  
+  })
 });
