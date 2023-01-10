639244493c76454b8fbe5c7744c043776bc67a58
lucasantoniodev <lucasantoniodev@gmail.com>
2023-01-10 13:15:51 -0300

    Category: DeleteLogicCategoriesRoute

--- docs/categories/categoriesPaths.ts
+++ docs/categories/categoriesPaths.ts
@@ -91,5 +91,82 @@ export const categoriesPaths = {
         403: responses[403],
       },
     },
+    delete: {
+      tags: ["Categories"],
+      summary: "Deletar por ID",
+      description: "Deleta uma categoria baseado no ID passado como parâmetro",
+      parameters: [
+        {
+          type: "string",
+          name: "id",
+          in: "path",
+          description: "ID da categoria",
+          required: true,
+        },
+      ],
+      responses: {
+        200: responses[200].category,
+        400: responses[400],
+        401: responses[401],
+        403: responses[403],
+      },
+    },
+  },
+  "/categories/guide/{id}": {
+    get: {
+      tags: ["Categories"],
+      summary: "Retorna todas as categorias vinculadas a um guia",
+      description: "Rota para retornar as categorias que contém o mesmo guia",
+      parameters: [
+        {
+          type: "string",
+          name: "id",
+          in: "path",
+          description: "ID do Guia",
+          required: true,
+        },
+      ],
+      responses: {
+        200: {
+          description: "Categorias da aplicação",
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  data: {
+                    type: "array",
+                    items: categoriesSchemas.dataResponse,
+                  },
+                },
+              },
+            },
+          },
+        },
+        400: responses[400],
+      },
+    },
+  },
+  "/categories/delete/{id}": {
+    patch: {
+      tags: ["Categories"],
+      summary: "Realiza o Delete Lógico pelo ID",
+      description: "Essa rota deleta logicamente uma categoria baseado no parâmetro passado",
+      parameters: [
+        {
+          type: "string",
+          name: "id",
+          in: "path",
+          description: "ID da categoria",
+          required: true,
+        },
+      ],
+      responses: {
+        200: responses[200].category,
+        400: responses[400],
+        401: responses[401],
+        403: responses[403],
+      },
+    },
   },
 };
